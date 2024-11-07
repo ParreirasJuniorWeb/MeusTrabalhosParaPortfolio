35a1f4b1d3b3f5769cd58ebd76976a545079e7b6
ParreirasJuniorWeb <153013106+ParreirasJuniorWeb@users.noreply.github.com>
2024-11-07 20:48:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
-const inputBox = document.getElementById("input-box"),
-listContainer = document.getElementById("list-container");
+const keyWebPageTemp = '6a98d033df7226983b3920af0398cfdd';
 
-function addTask() {
-    if(inputBox.value === ''){
-        alert("You must write something!");
-    }else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+async function clickButton() {
 
-        let span = document.createElement('span');
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = "";
-    saveData();
+    const cidade = document.querySelector(".input-cidade").value
+    //alert(cidade)  
+    
+    //API fetch 
+
+    const dados = await //Server para aguardar o resultado da consulta ao servidor - API request.
+    
+    fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${keyWebPageTemp}&lang=pt_br&units=metric`
+    ).then(response => response.json())
+
+    //Resposta na tela do usuário via nossa aplicação
+
+    console.log(dados)
+    //alert(dados.name, dados.main.temp+ "°C", dados.weather[0].description, dados.main.humidity+ "%") Não deu certo com o Alert!
+        
+    //Resposta na aplicação em seus respectivos campos
+
+    document.querySelector('.cidade').textContent = "Tempo em "+dados.name
+
+    document.querySelector('.temp').textContent = Math.floor(dados.main.temp) + "°C" //Arredonda o número
+
+    document.querySelector('.texto-previsao').textContent = dados.weather[0].description
+
+    document.querySelector('.umidade').textContent = "Umidade: ".dados.main.humidity + "%"
+
+    document.querySelector('.img-previsao').scr=`https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+
+    // const btn_buscar = document.querySelector(".btn-buscar")  --- Busca via Teclado, pressionando a tecla "Enter"
+
+    // btn_buscar.addEventListener("Keydown", function(event){
+    //     if (event.key === "Enter"){
+
+    //         clickButton();
+
+    //     }
+    //   })
 }
-
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === 'LI'){
-        e.target.classList.toggle("checked");
-        saveData();
-
-    } else if (e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false);
-
-function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML);
-}
-function showTask() {
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-showTask();
